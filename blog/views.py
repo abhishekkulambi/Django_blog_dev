d357d593675c037e8503910acba8446c5b9282f0
abhishekkulambi <abhikulambi@gmail.com>
2021-05-30 20:33:37 +0530

    Second Blog commit

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
+#dummy post data
+posts = [
+		{
+			'author': 'Abhishek KS',
+			'title': 'Blog Post 1',
+			'content': 'First blog post',
+			'date_posted': 'May 29, 2020'
+
+		},
+		{
+			'author': 'Abhishek KS',
+			'title': 'Blog Post 2',
+			'content': 'Second blog post',
+			'date_posted': 'May 30, 2020'
+		}
+]
+
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html', {'title': 'About'})
