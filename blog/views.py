cc48a107aa53291b4e83eac5516496b49d8d09d1
abhishekkulambi <abhikulambi@gmail.com>
2021-06-03 14:14:35 +0530

    Display blogs in admin panel

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,9 @@
 from django.shortcuts import render
-
-
-# Create your views here.
-#dummy post data
-posts = [
-		{
-			'author': 'Abhishek KS',
-			'title': 'Blog Post 1',
-			'content': 'First blog post',
-			'date_posted': 'May 29, 2020'
-
-		},
-		{
-			'author': 'Abhishek KS',
-			'title': 'Blog Post 2',
-			'content': 'Second blog post',
-			'date_posted': 'May 30, 2020'
-		}
-]
+from .models import Post
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
